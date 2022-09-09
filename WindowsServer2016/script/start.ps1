69bbfdc07fcc3cc7f495abe9eeaa0db09fae9054
mkazuyuki <kazuyuki@nec.com>
2022-09-09 19:09:09 +0900

    Update start.ps1

--- WindowsServer2016/script/start.ps1
+++ WindowsServer2016/script/start.ps1
@@ -3,6 +3,9 @@
 #
 $ErrorStart = 1
 $ErrorRemove = 3
+$ErrorImport = 5
+$ErrorGetVHD = 6
+$ErrorIcacls = 7
 
 Write-Output "=== Get-VM ===" 
 Get-VM -Name $env:VM
@@ -56,6 +59,60 @@ $bRet = $?
 if ($bRet -eq $False)
 {
         Write-Output "Import-VM failed."
+
+        Write-Output "=== robocopy ==="
+        Write-Output "SourcePaht: $env:DestPath"
+        Write-Output "DestPaht  : $env:SourcePath"
+        robocopy $env:DestPath $env:SourcePath /MIR
+        $bRet = $LASTEXITCODE
+        if ($bRet -ge 8)
+        {
+                Write-Output "robocopy failed (ret: $bRet)."
+                exit $ErrorCopy
+        }
+        else
+        {
+                Write-Output "robocopy succeeded."     
+        }
+
+        while ($True) {
+            # On WS2019, nothing happened just by copying the DestPath to the SourcePath,
+            # and so, retrying importing operation.
+            Write-Output "=== Import-VM ==="
+            Write-Output "Retry import $env:VMCX2"
+            Import-VM -Path $env:VMCX2 -Confirm:$False
+
+            Write-Output "=== Get-VM ==="
+            Get-VM -Name $env:VM
+            if ($? -eq $True)
+            {
+                break
+            }
+
+            # Slow down output for scrpl*.log
+            sleep 10
+        }
+        Write-Output "${env:VM} became available."
+
+        Write-Output "=== Get-VHD ==="
+        $vhdlist = Get-VHD -VMId $env:ID
+        if ($vhdlist -eq $null)
+        {
+            Write-Output "Get-VHD failed. Please check the VMID in the configuration file or if there is virtual hard disks."
+            exit $ErrorGetVHD
+        }
+
+        Write-Output "=== Grant the permission to VHDs ==="
+        foreach ($vhd in $vhdlist)
+        {
+            $tmp = "NT VIRTUAL MACHINE\" + $env:ID
+            icacls $vhd.Path /grant ${tmp}:`(r,w`)
+            if ($? -eq $False)
+            {
+                Write-Output "icacls failed. Please check the VMID in the configuration file or if there is virtual hard disks."
+                exit $ErrorIcacls
+            }
+        }
 }
 else
 {
